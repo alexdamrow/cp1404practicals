9c25783d3b2bdfd951660fef254bc4e5a2153c3b
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-06-07 18:16:05 +1000

    Add determine grade function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -2,6 +2,8 @@ MENU = """(G)et
 (P)rint
 (S)how
 (Q)uit"""
+
+
 def main():
     """Interactive menu."""
     print(MENU)
@@ -10,13 +12,15 @@ def main():
         if choice == "G":
             score = get_valid_input("Enter score: ", 0, 100)
         elif choice == "P":
-
+            print(determine_result(score))
         elif choice == "S":
 
         else:
             print("Invalid ")
+        print(MENU)
         choice = input(">>>").upper()
     print("Farewell")
+
 
 def get_valid_input(prompt, min_number, max_number):
     """Get a valid input."""
@@ -27,3 +31,18 @@ def get_valid_input(prompt, min_number, max_number):
     return score
 
 
+def determine_result(score):
+    """Determine result based off score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Pass"
+    else:
+        return "Excellent"
+
+
+
+
+main()
