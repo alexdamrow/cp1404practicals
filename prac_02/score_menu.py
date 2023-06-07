9cf40669a5760b6f3204b012a8399310a15afce2
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-06-07 18:08:34 +1000

    Add checking input function and structure menu

---
+++ prac_02/score_menu.py
@@ -0,0 +1,29 @@
+MENU = """(G)et 
+(P)rint
+(S)how
+(Q)uit"""
+def main():
+    """Interactive menu."""
+    print(MENU)
+    choice = input(">>>").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score("Enter score: ", 0, 100)
+        elif choice == "P":
+
+        elif choice == "S":
+
+        else:
+            print("Invalid ")
+        choice = input(">>>").upper()
+    print("Farewell")
+
+def get_valid_input(prompt, min_number, max_number):
+    """Get a valid input."""
+    score = int(input(prompt))
+    while score < min_number or score > max_number:
+        print("Invalid input")
+        score = int(input(prompt))
+    return score
+
+
