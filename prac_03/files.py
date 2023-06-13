a1e52f58ef9b0d331a1983b10af87b7f49928318
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-06-13 11:47:42 +1000

    Add question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,14 +1,24 @@
-def question_1():
-    name = input("Name: ")
-    out_file = open("name.txt", 'w')
-    print(name, file=out_file)
-    out_file.close()
+# Question 1
 
-def question_2():
-    in_file = open("name.txt", 'r')
-    name = in_file.read()
-    in_file.close()
-    print(name)
+name = input("Name: ")
+out_file = open("name.txt", 'w')
+print(name, file=out_file)
+out_file.close()
+
+# Question 2
+
+in_file = open("name.txt", 'r')
+name = in_file.read()
+in_file.close()
+print(f"Your name is {name}")
+
+# Question 3
+
+in_file = open("numbers.txt", 'r')
+first_number = int(in_file.readline())
+second_number = int(in_file.readline())
+in_file.close()
+print(first_number + second_number)
 
 
 
@@ -17,7 +27,3 @@ def question_2():
 
 
 
-
-
-# question_1()
-question_2()
