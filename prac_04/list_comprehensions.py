b64e8f9ab034295293e73ac8a95b50a8aa2b6ae1
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-06-21 16:38:20 +1000

    Complete third todo

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -36,8 +36,10 @@ almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 numbers = [int(number) for number in almost_numbers]
 print(numbers)
 
-# TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+greater_than_nine = [number for number in numbers if number > 9]
+print(greater_than_nine)
+
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
