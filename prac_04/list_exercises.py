dc5d469fd6041b522d2214fa82ea50e4362b757c
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-06-23 12:36:35 +1000

    Add comments to seperate questions

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,3 +1,5 @@
+# Question 1
+
 AMOUNT_OF_NUMBERS = 5
 numbers = []
 for i in range(AMOUNT_OF_NUMBERS):
@@ -8,6 +10,8 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers)/ AMOUNT_OF_NUMBERS}")
+
+# Question 2
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
