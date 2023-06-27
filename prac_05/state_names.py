9a96fc20a108b68a032e83c150831621cf6a2ceb
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-06-27 16:53:26 +1000

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,10 +9,23 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
+state_name_length = 0
+max_length = max(len(state_name) for state_name in list(CODE_TO_NAME.values()))
+for state_code, state_name in CODE_TO_NAME.items():
+    print(f"{state_code:3} is {state_name:{max_length}}")
+
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper
+    state_code = input("Enter short state: ").upper()
+
+
+# while state_code != "":
+#     if state_code in CODE_TO_NAME:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
