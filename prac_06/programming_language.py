b020a38de689f9f49fbedbdc9572762968b69e55
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-07-12 16:44:16 +1000

    Add programming_language.py program and __init__ method

---
+++ prac_06/programming_language.py
@@ -0,0 +1,8 @@
+"""CP1404 Practical - programming language class
+Estimated time: 30 minutes
+Actual time: """
+class ProgrammingLanguage:
+    def __init__(self, typing="", reflection="", year=0.0):
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
