e5c4a4ecb1e6aa1bf7a91107bf40c9f09893bbd2
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-07-12 16:51:54 +1000

    Add name field to ProgrammingLanguage

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,8 +1,17 @@
 """CP1404 Practical - programming language class
 Estimated time: 30 minutes
 Actual time: """
+
+
 class ProgrammingLanguage:
-    def __init__(self, typing="", reflection="", year=0.0):
+    def __init__(self, name="", typing="", reflection="", year=0):
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
