318d52155dee81351ca8ba06e9b6517b2c6997f5
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-07-13 21:53:13 +1000

    Add doc strings to ProgrammingLanguage and is_dynamic in programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,10 +1,17 @@
 """CP1404 Practical - programming language class
 Estimated time: 30 minutes
-Actual time: """
+Actual time: 35 minutes """
 
 
 class ProgrammingLanguage:
     def __init__(self, name="", typing="", reflection="", year=0):
+        """
+        Initialise a Programming language instance.
+        name: string, the name of the programming language
+        typing: string, is the language dynamic or static
+        reflection: string, the language is reflective
+        year: integer, year that language was first used
+        """
         self.name = name
         self.typing = typing
         self.reflection = reflection
@@ -14,6 +21,7 @@ class ProgrammingLanguage:
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
+        """Returns true or false if language is dynamic."""
         if self.typing == "Dynamic":
             return True
         else:
