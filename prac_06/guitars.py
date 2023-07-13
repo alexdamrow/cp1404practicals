d3aa9b81ab2c1f4cd1e2f8712a6870ad649ac8b8
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-07-13 22:55:38 +1000

    Add for loop to print guitars in guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,7 +5,11 @@ print("My guitars!")
 name = input("Name: ")
 while name != "":
     year = int(input("Year: "))
-    cost = int(input("Cost: $"))
+    cost = float(input("Cost: $"))
     guitars.append(Guitar(name, year, cost))
     print("")
     name = input("Name: ")
+print("These are my guitars: ")
+for i, guitar in enumerate(guitars):
+    vintage_string = guitar.is_vintage()
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
