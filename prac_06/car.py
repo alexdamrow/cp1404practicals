5f23c72e41bb38083fc58ed00b8ded10c6074a0f
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-07-14 11:03:06 +1000

    Add name field to doc string in init in car.py

--- prac_06/car.py
+++ prac_06/car.py
@@ -8,13 +8,14 @@ class Car:
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
+        name: string, cars name
         """
         self.name = name
         self.fuel = fuel
         self._odometer = 0
+
     def __str__(self):
         return f"{self.name}, fuel ={self.fuel}, odometer={self._odometer} "
-
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
