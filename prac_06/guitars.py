e0a68579bf99e38f8f5dba6972f6857a2582c759
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-07-16 18:19:47 +1000

    Wrap code in main function, add print statement in while loop and retool vintage_string in guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,15 +1,23 @@
 from prac_06.guitar import Guitar
 
-guitars = []
-print("My guitars!")
-name = input("Name: ")
-while name != "":
-    year = int(input("Year: "))
-    cost = float(input("Cost: $"))
-    guitars.append(Guitar(name, year, cost))
-    print("")
+
+def main():
+    guitars = []
+    print("My guitars!")
     name = input("Name: ")
-print("These are my guitars: ")
-for i, guitar in enumerate(guitars):
-    vintage_string = guitar.is_vintage()
-    print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:.2f} added.")
+        print("")
+        name = input("Name: ")
+    print("These are my guitars: ")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = ""
+        if guitar.is_vintage():
+            vintage_string = "(vintage)"
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
+
+
+main()
