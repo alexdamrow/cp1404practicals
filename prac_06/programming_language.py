55f21c951554a6817d18286011c3c091f4752882
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-07-16 18:46:35 +1000

    Add doc string to __str__ and change the type for the type reflection  in programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -9,7 +9,7 @@ class ProgrammingLanguage:
         Initialise a Programming language instance.
         name: string, the name of the programming language
         typing: string, is the language dynamic or static
-        reflection: string, the language is reflective
+        reflection: bool, the language is reflective
         year: integer, year that language was first used
         """
         self.name = name
@@ -18,6 +18,7 @@ class ProgrammingLanguage:
         self.year = year
 
     def __str__(self):
+        """Return string representation of data in a ProgrammingLanguage object."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
