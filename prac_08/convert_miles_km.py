514477da9548da7b46fb36d7d66d9e212e8abbca
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-07-27 14:51:08 +1000

    Add the functions handle_calculation and handle_increment to convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,14 +6,29 @@ Actual time:
 """
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+FACTOR_MILES_TO_KM = 1.60934
 
 
 class ConvertMilesKmApp(App):
+    output_km = StringProperty()
 
     def build(self):
+        """Construct the app."""
         self.title = "Miles and Km converter"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_calculations(self, text):
+        """Handle the calculations."""
+        miles = float(text)
+        self.output_km = str(miles * FACTOR_MILES_TO_KM)
+
+    def handle_increment(self, text, change):
+        """Handle the up and down button and update the input."""
+        miles = float(text) + change
+        self.root.ids.input_number.text = str(miles)
 
 
 
