285e7ed64fb707c2d655747462117fe716441f37
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-08-01 11:37:01 +1000

    Update print statement in taxi_test.py

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,12 +3,12 @@ from taxi import Taxi
 
 def main():
     """Test the class Taxi."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
-    print(f"{my_taxi}, {my_taxi.get_fare()}")
+    print(f"{my_taxi}, costs ${my_taxi.get_fare()}")
     my_taxi.start_fare()
     my_taxi.drive(100)
-    print(f"{my_taxi}, {my_taxi.get_fare()}")
+    print(f"{my_taxi}, costs ${my_taxi.get_fare()}")
 
 
 main()
