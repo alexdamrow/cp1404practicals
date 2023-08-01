697830edb644e9a4b14268b3aa4a0ccfd700c133
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-08-01 16:43:42 +1000

    Delete drive and start_fare method and rework get_fare method in silver_service_taxi.py

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -14,22 +14,13 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
-        self.current_fare_distance = 0
 
     def __str__(self):
         """Return a string like a Taxi, but with a flag fall"""
-        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
 
     def get_fare(self):
         """Return price from trip."""
-        return self.price_per_km * self.start_fare()
+        return self.flagfall + super().get_fare()
 
-    def start_fare(self):
-        """Begin a new fare."""
-        self.current_fare_distance = 0
 
-    def drive(self, distance):
-        """Drive like parent class Taxi."""
-        distance_driven = super().drive(distance)
-        self.current_fare_distance += distance_driven
-        return distance_driven
